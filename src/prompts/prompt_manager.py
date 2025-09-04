aa1f3984341d11d13f74ada1cf4187c9c0dfa6ff
koiljat <koiljat@u.nus.edu>
2025-09-04 20:59:16 +0800

    refactor: prompt templates

---
+++ src/prompts/prompt_manager.py
@@ -0,0 +1,20 @@
+import yaml
+from langchain.prompts import PromptTemplate
+
+class PromptManager:
+    def __init__(self, filepath: str="src/prompts/prompts.yaml"):
+        with open(filepath, "r") as f:
+            self.prompts = yaml.safe_load(f)
+
+    def get(self, key: str) -> PromptTemplate:
+        if key not in self.prompts:
+            raise ValueError(f"Prompt '{key}' not found in prompts.yaml")
+        return PromptTemplate(
+            input_variables=["text"],
+            template=self.prompts[key]
+        )
+
+if __name__ == "__main__":
+    pm = PromptManager()
+    prompt = pm.get("default").format(text="This is a test")
+    print(prompt)
